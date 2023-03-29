7145bac4109b64c71500b24f8e27c7cd2a0ce910
Lőrincz Dániel <dani.lorincz@gmail.com>
2023-03-30 01:38:08 +0200

    V 4.3 INTRO

---
+++ V4.3_INTRO.h
@@ -0,0 +1,5 @@
+Utolsó változtatások
+    //! a HEAD oldalfalának megvastagítása a nyomtatási deformáció csökkentése érdekében
+    //! a HEAD-ben a contact pcb helyét kicsit meg kéne dönteni hogy könnyebben belesiklódjon a konnektor
+    //! szenzornak nincs elegendően nagy helye a COVER-en hogy bemenjen helyére rendesen (csavar fej ütközik) 
+    //! a szenzornak kell egy tok
