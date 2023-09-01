78a73956a844e1d01d743485faf3d6a3824f1263
Lőrincz Dániel <dani.lorincz@gmail.com>
2023-09-01 10:48:03 +0200

    Pretify webpage

--- headShield_V4.3.2/hardware/Battery.h
+++ headShield_V4.3.2/hardware/Battery.h
@@ -55,6 +55,11 @@ public:
     {
         float voltage = getVoltage();
         percent = map(voltage * 100, 6 * 100, 8.4 * 100, 0, 100);
+        /*
+        if (percent > 100)
+            percent = 100;
+        else if (percent < 1)
+            percent = 1;*/
         return percent;
     }
 };
--- headShield_V4.3.2/webpage.h
+++ headShield_V4.3.2/webpage.h
@@ -1,54 +1,44 @@
 const char *webpageCode = R"=====(
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
     <title>Premet Powered Air Purifying Respirator</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-            background-color: #f4f4f4;
-        }
         .card {
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 4px 8px rgba(0,0,0,0.1);
-            margin-bottom: 20px;
-            padding: 5px;  /* Reduced padding */
-            display: inline-block;  /* Added */
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            max-width: 300px;
+            margin: auto;
+            text-align: center;
+            padding: 10px;
+            border-radius: 12px;  /* Rounded corners */
         }
-        h2 {
-            margin: 0;  /* Added */
-            padding: 0;  /* Added */
-        }
-        p {
-            margin: 5px 0;  /* Modified */
+        h1 {
+            text-align: center;
         }
     </style>
 </head>
 <body>
 
-<h1>Premet Powered Air Purifying Respirator</h1>
+<h1>Premet Powered<br>Air Purifying Respirator</h1>
 
 <div class="card">
-    <h2>State & Battery</h2>
-    <p>State: <span id="state"></span></p>
-    <p>Battery: <span id="battery"></span></p>
+    <h2>System</h2>
+    <p id="state">State: </p>
+    <p id="battery">Battery: </p>
 </div>
 
 <div class="card">
-    <h2>Environmental Data</h2>
-    <p>Temperature: <span id="temp"></span></p>
-    <p>Pressure: <span id="press"></span></p>
-    <p>Humidity: <span id="humi"></span></p>
+    <h2>Enviromental</h2>
+    <p id="temp">Temperature: </p>
+    <p id="press">Pressure: </p>
+    <p id="humi">Humidity: </p>
 </div>
 
 <div class="card">
     <h2>Air Quality</h2>
-    <p>AQI: <span id="AQI"></span></p>
-    <p>TVOC: <span id="TVOC"></span></p>
-    <p>ECO2: <span id="ECO2"></span></p>
+    <p id="AQI">AQI: </p>
+    <p id="TVOC">TVOC: </p>
+    <p id="ECO2">ECO2: </p>
 </div>
 
 <script>
@@ -56,14 +46,14 @@ const char *webpageCode = R"=====(
         fetch('/helmetData')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('state').innerText = data.state;
-            document.getElementById('battery').innerText = data.battery;
-            document.getElementById('temp').innerText = data.temp;
-            document.getElementById('press').innerText = data.press;
-            document.getElementById('humi').innerText = data.humi;
-            document.getElementById('AQI').innerText = data.AQI;
-            document.getElementById('TVOC').innerText = data.TVOC;
-            document.getElementById('ECO2').innerText = data.ECO2;
+            document.getElementById('state').innerText = "State: " + data.state;
+            document.getElementById('battery').innerText = "Battery: " + data.battery + "%";
+            document.getElementById('temp').innerText = "Temperature: " + data.temp + " Celsius";
+            document.getElementById('press').innerText = "Pressure: " + data.press + " Pa";
+            document.getElementById('humi').innerText = "Humidity: " + data.humi + "%";
+            document.getElementById('AQI').innerText = "AQI: " + data.AQI;
+            document.getElementById('TVOC').innerText = "TVOC: " + data.TVOC + " ppb";
+            document.getElementById('ECO2').innerText = "ECO2: " + data.ECO2 + " ppm";
         });
     }
 
