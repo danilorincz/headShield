424c8a540e444c0f849fa259dd45f65b5c281211
Lőrincz Dániel <dani.lorincz@gmail.com>
2023-09-01 15:48:51 +0200

    Battery indicator work perfetctly

--- headShield_V4.3.2/webpage.h
+++ headShield_V4.3.2/webpage.h
@@ -3,45 +3,53 @@ const char *webpageCode = R"=====(
 <html>
 <head>
     <title>Premet Powered Air Purifying Respirator</title>
-    <style>
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            max-width: 300px;
-            margin: auto;
-            text-align: center;
-            padding: 10px;
-            border-radius: 12px;  /* Rounded corners */
-        }
-        h1 {
-            text-align: center;
-        }
-        #stateText {
-            display: inline-block;
-            padding: 4px 12px;
-            border-radius: 12px;
-            font-size: 1.5em;
-        }
-/* New Styles for Battery Icon */
-        .battery {
-            width: 50px;
-            height: 20px;
-            border: 2px solid black;
-            position: relative;
-        }
-        .battery:before {
-            content: "";
-            width: 5px;
-            height: 14px;
-            position: absolute;
-            top: 3px;
-            left: 100%;
-            background-color: black;
-        }
-        .battery-level {
-            height: 100%;
-            background-color: green;
-        }
-    </style>
+<style>
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        max-width: 300px;
+        margin: auto;
+        text-align: center;
+        padding: 10px;
+        border-radius: 12px;  /* Rounded corners */
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    h1 {
+        text-align: center;
+    }
+    #stateText {
+        display: inline-block;
+        padding: 4px 12px;
+        border-radius: 12px;
+        font-size: 1.5em;
+    }
+    .battery {
+        width: 50px;
+        height: 20px;
+        border: 2px solid black;
+        position: relative;
+    }
+
+    .battery:before {
+        content: "";
+        width: 5px;
+        height: 14px;
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        background-color: black;
+        transform: translateY(-50%);
+    }
+
+    .battery-level {
+        height: 100%;
+        color: white;
+        font-weight: bold;
+        text-align: center;  /* To center the text */
+    }
+</style>
+
 </head>
 <body>
 
@@ -113,6 +121,7 @@ const char *webpageCode = R"=====(
             let batteryLevel = document.getElementById('batteryLevel');
             let percent = data.battery;
             batteryLevel.style.width = `${percent}%`;
+            batteryLevel.innerText = `${percent}%`; // Displaying percentage inside the indicator
             if (percent > 50) {
                 batteryLevel.style.backgroundColor = 'green';
             } else if (percent > 20) {
@@ -120,6 +129,7 @@ const char *webpageCode = R"=====(
             } else {
                 batteryLevel.style.backgroundColor = 'red';
             }
+
 
             // Updating Other Data
             document.getElementById('temp').innerText = `Temperature: ${data.temp} Celsius`;
