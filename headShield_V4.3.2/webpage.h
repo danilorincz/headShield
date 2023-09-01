166b59f52c29eb4fed2e095049ae4b426c7dccdd
Lőrincz Dániel <dani.lorincz@gmail.com>
2023-09-01 15:14:44 +0200

    Working webpage code, all the data are shown

--- headShield_V4.3.2/webpage.h
+++ headShield_V4.3.2/webpage.h
@@ -21,6 +21,26 @@ const char *webpageCode = R"=====(
             border-radius: 12px;
             font-size: 1.5em;
         }
+/* New Styles for Battery Icon */
+        .battery {
+            width: 50px;
+            height: 20px;
+            border: 2px solid black;
+            position: relative;
+        }
+        .battery:before {
+            content: "";
+            width: 5px;
+            height: 14px;
+            position: absolute;
+            top: 3px;
+            left: 100%;
+            background-color: black;
+        }
+        .battery-level {
+            height: 100%;
+            background-color: green;
+        }
     </style>
 </head>
 <body>
@@ -30,7 +50,10 @@ const char *webpageCode = R"=====(
 <div class="card">
     <h2>System</h2>
     <p id="state">State: <span id="stateText"></span></p>
-    <p id="battery">Battery: </p>
+    <div class="battery">
+        <div id="batteryLevel" class="battery-level"></div>
+    </div>
+    <p id="batteryPercent"></p>
 </div>
 
 <div class="card">
@@ -52,8 +75,7 @@ const char *webpageCode = R"=====(
         fetch('/helmetData')
         .then(response => response.json())
         .then(data => {
-            // ... Existing Code ...
-
+            // Handling the State
             let stateText = document.getElementById('stateText');
             switch (data.state) {
                 case 0:
@@ -86,12 +108,33 @@ const char *webpageCode = R"=====(
                     stateText.style.backgroundColor = "gray";
                     stateText.style.color = "white";
             }
+
+            // Handling the Battery
+            let batteryLevel = document.getElementById('batteryLevel');
+            let percent = data.battery;
+            batteryLevel.style.width = `${percent}%`;
+            if (percent > 50) {
+                batteryLevel.style.backgroundColor = 'green';
+            } else if (percent > 20) {
+                batteryLevel.style.backgroundColor = 'yellow';
+            } else {
+                batteryLevel.style.backgroundColor = 'red';
+            }
+
+            // Updating Other Data
+            document.getElementById('temp').innerText = `Temperature: ${data.temp} Celsius`;
+            document.getElementById('press').innerText = `Pressure: ${data.press} Pa`;
+            document.getElementById('humi').innerText = `Humidity: ${data.humi} %`;
+            document.getElementById('AQI').innerText = `AQI: ${data.AQI}`;
+            document.getElementById('TVOC').innerText = `TVOC: ${data.TVOC} ppb`;
+            document.getElementById('ECO2').innerText = `ECO2: ${data.ECO2} ppm`;
         });
     }
 
-    // Fetch data every 2 seconds
+    // Fetch the data every 2 seconds
     setInterval(fetchData, 2000);
 </script>
+
 
 </body>
 </html>
