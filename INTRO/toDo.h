cff743d2fdeb599805df2ba5acd37bb3d0d32849
Lőrincz Dániel <dani.lorincz@gmail.com>
2023-09-30 02:27:02 +0200

    Last toDo update

--- INTRO/toDo.h
+++ INTRO/toDo.h
@@ -1,13 +1,29 @@
-fej egyenes, szűrők fent: 3590-3610
-fej lehajtva, szűrők fent max: 3628
-fej egyenes, tömítés min-max: 3624-3636
-fej lehajtva, tömítés min-max: 3638-3650
-
-
-fej egyenes:
-szűrők fent min-max: 3586-3006
-tömítés min-max: 3617-3638
-
-fej lehajtva:
-szűrők fent min-max:3592-2617
-tömítés min-max: 3632-3648
+// MECHANIKA
+    // megpróbálni nyomtatni egy nagyon tömör filter washer-t
+    // szétszerelni az összes sisakot
+    // kitisztítani
+    // átnézni a csavarokat
+    // ha szükséges, akkor a szilikont újra kell húzni
+    // utoljára polírozni ahol szükséges
+    // bekenni parafin olajjal
+    // minden sisakon kicserélni a PETG-t
+    // írni a Timinek hogy csináljon még kettő kámzsát
+// PROGRAMOZÁS
+    // minden sisakot tesztelni hogy ugyan azok maradtak-e a határértékek
+    // ellenőrizni a filter trackert mert szerintem nem működik
+    // elgondolkodni egy újabb adaptív féle védőhálón
+    // ki kell venni a ventillátor kikapcsolásának lehetőségét
+    // figyelmeztető rendszert élesíteni
+// DOKUMENTÁCIÓ
+    // új rajzot készíteni a fém részről, és a COVER összeállítási rajzát frirssíteni
+    // a matricáról műszaki rajz
+    // használati útmutatót frissíteni
+        // hogyan teszteljük
+        // mindig úgy tároljuk hogy a VISOR le van hajtva
+        // ha asztalon fekszik a készülék akkor a VISOR mindig legyen a felső pozíciójában
+    // csomagolási útmutató
+    // mi van a csomagban leírás
+    // Zsoltéknak írni hogy hogyan teszteljék
+    // utoljára átnézni a dokumentációt
+    // +hozzáadni egy plusz mérési jegyzőkönyvet excel formában
+// minden akksi legyen feltöltve
